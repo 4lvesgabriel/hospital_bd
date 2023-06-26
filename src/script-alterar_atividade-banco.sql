6cf23be652684ae5f246b65e692555177e233c2f
Gabriel Alves Gabriel <101440339+4lvesgabriel@users.noreply.github.com>
2023-06-26 00:45:39 -0300

    Deletando códigos antigos

--- src/script-alterar_atividade-banco.sql
+++
@@ -1,9 +0,0 @@
-USE hospital_fundamental;
-
-ALTER TABLE medico ADD em_atividade varchar(255);
-
-UPDATE medico SET em_atividade = "inativo" WHERE id_medico = 1;
-UPDATE medico SET em_atividade = "inativo" WHERE id_medico = 2;
-UPDATE medico SET em_atividade = "ativo" WHERE id_medico != 1 AND id_medico !=2;
-
-SELECT * FROM medico;
